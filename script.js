f0115d525e2f82822371970619799a791e234ef3
Roy Lopez <windup.dues-0f@icloud.com>
2022-10-12 20:23:02 -0300

    javascript ready

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+const container = document.getElementById("container");
+const circleText = document.getElementById("text");
 
+const totalTime = 7500;
+const breathTime = (totalTime / 5) * 2;
+const holdTime = totalTime / 5;
+
+breathAnimation();
+
+function breathAnimation() {
+  circleText.innerText = "Breath In";
+  container.className = "container grow";
+
+  setTimeout(() => {
+    circleText.innerText = "Hold";
+    setTimeout(() => {
+      circleText.innerText = "Breath Out";
+      container.className = "container shrink";
+    }, holdTime);
+  }, breathTime);
+}
+
+setInterval(breathAnimation, totalTime);
